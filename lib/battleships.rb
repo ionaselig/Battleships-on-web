23353a691310c244458044fe31f9fcdd443b9db3
Jeremy Fox <foxjerem@gmail.com>
2014-07-21 14:28:25 +0100

    Added HTML files index and name_check

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -2,8 +2,12 @@ require 'sinatra/base'
 
 class BattleShips < Sinatra::Base
   get '/' do
-    'Hello BattleShips!'
+    erb :index
   end
+
+  get '/name_check' do
+  	erb :new_game
+	end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
