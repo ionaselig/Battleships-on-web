e2825ce194d8aa09217b4d9e929f7d6d78463215
Jeremy Fox <foxjerem@gmail.com>
2014-07-22 12:17:35 +0100

    Added concept of a game sesssion

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -2,20 +2,35 @@
 require 'sinatra/base'
 
 class BattleShips < Sinatra::Base
-  
+
+  enable :sessions
+  set :session_secret, "My session secret"
+
   get '/' do
+    session[:players] = []
+    session[:greeting] = "What's your name?"
     erb :index
   end
 
   get '/name_input' do
+    @greeting = session[:greeting]
   	erb :new_game
 	end
 
 	post '/init_game' do
 		@name = params[:player]
     redirect '/name_input' if @name.empty?
-		erb :init_game
+    session[:greeting] = "Welcome #{@name} please enter your opponent"
+    session[:players] <<  params[:player]
+    puts session[:players].inspect
+    redirect '/launch_game' if session[:players].count == 2
+		redirect '/name_input'
 	end
+
+  # get '/init_game' do
+  #   sleep 5
+  #   redirect '/name_input'
+  # end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
